ab488500d9a59bc9f0f29fc4593945a5b46e29a6
BrkGl <bgburakgel@gmail.com>
2025-05-27 00:28:57 +0300

    firstProject

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,8 +1,6 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
