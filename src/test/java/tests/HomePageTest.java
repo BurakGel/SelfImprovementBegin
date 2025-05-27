e0ca8c01dd2dc7d2642b1e7461a262d7faafdc39
BrkGl <bgburakgel@gmail.com>
2025-05-27 22:54:47 +0300

    firstProject

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -7,18 +7,16 @@ import org.testng.annotations.Test;
 public class HomePageTest extends BaseTest {
 
     @Test
-    public void searchLaptopOnTrendyol() {
-        // Google anasayfası baseUrl olarak config'de tanımlı olmalı (örnek: https://www.google.com)
+    public void seleniumAutomationFirst() {
 
-        // Arama kutusunu bul (Google'ın arama kutusu name="q" ile bulunur)
-        driver.findElement(By.id("firstname")).sendKeys("Burak");
-        driver.findElement(By.id("surname")).sendKeys("Gel");
+        driver.findElement(By.id("firstName")).sendKeys("Burak");
+        driver.findElement(By.id("surName")).sendKeys("Gel");
         new Select(driver.findElement(By.id("gender"))).selectByValue("male");
         driver.findElement(By.id("red")).click();
         driver.findElement(By.cssSelector("#contactus > div:nth-child(5) > div > label > textarea")).sendKeys("easy peasy lemon squezy");
         new Select(driver.findElement(By.id("continent"))).selectByValue("asia");
-        driver.findElement(By.id("submitbutton")).click();
-        System.out.println("Test başarıyla tamamlandı");
+        driver.findElement(By.id("submitButton")).click();
+        System.out.println("Test basariyla tamamlandi");
 
 
     }
